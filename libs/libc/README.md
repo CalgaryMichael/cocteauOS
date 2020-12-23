7200eb3d8c134bbd31c5bc65fb13e8a360c37c31
Calgary Michael <cseth.michael@gmail.com>
2020-12-23 16:06:53 -0500

    add libc

---
+++ libs/libc/README.md
@@ -0,0 +1,8 @@
+# libc
+
+This contains both the code for `libc` (hosted) and `libk` (freestanding).
+
+Ported from [osdev.org's Meaty Skeleton](https://wiki.osdev.org/Meaty_Skeleton)
+
+**Note**: `libc` is currently _not_ working.
+
---
+++ libs/libc/stdio/printf.libk.d
@@ -0,0 +1,6 @@
+stdio/printf.libk.o: stdio/printf.c \
+ /usr/local/Cellar/i686-elf-gcc/10.2.0/lib/gcc/i686-elf/10.2.0/include-fixed/limits.h \
+ /usr/local/Cellar/i686-elf-gcc/10.2.0/lib/gcc/i686-elf/10.2.0/include/stdbool.h \
+ /usr/local/Cellar/i686-elf-gcc/10.2.0/lib/gcc/i686-elf/10.2.0/include/stdarg.h \
+ include/stdio.h include/sys/cdefs.h include/string.h \
+ /usr/local/Cellar/i686-elf-gcc/10.2.0/lib/gcc/i686-elf/10.2.0/include/stddef.h
