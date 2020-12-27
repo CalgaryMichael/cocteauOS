863f35ab493da8e526815f20e1888c684883178f
Calgary Michael <cseth.michael@gmail.com>
2020-12-27 17:38:36 -0500

    rewrite terminal output

--- libs/kernel/arch/i386/tty.c
+++ libs/kernel/arch/i386/tty.c
@@ -1,43 +1,60 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-//#include <string.h>
+#include <string.h>
 
 #include <kernel/tty.h>
 
-static char *vidptr = (char*)0xb8000; 	//video mem begins here.
+#include "vga.h"
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+static uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
+ 
+static size_t terminal_row;
+static size_t terminal_column;
+static uint8_t terminal_color;
+static uint16_t* terminal_buffer;
+
+
+void set_buffer(unsigned char c, uint8_t color, size_t x, size_t y) {
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
+	return;
+}
 
 void terminal_init(void) {
-	unsigned int i = 0;
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = VGA_MEMORY;
 
-	/* this loops clears the screen
-	* there are 25 lines each of 80 columns; each element takes 2 bytes */
-	while(i < 80 * 25 * 2) {
-		/* blank character */
-		vidptr[i] = ' ';
-		/* attribute-byte - light grey on black screen */
-		vidptr[i+1] = 0x07; 		
-		i = i + 2;
+	unsigned char blank_char = ' ';
+
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			set_buffer(blank_char, terminal_color, x, y);
+		}
 	}
-
 	return;
 }
 
 void terminal_putchar(char c) {
+	unsigned char uc = c;
+	set_buffer(uc, terminal_color, terminal_column, terminal_row);
+	if (++terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT) {
+			terminal_row = 0;
+		}
+	}
 	return;
 }
 
 void terminal_out(const char* data) {
-	unsigned int i = 0;
-	unsigned int j = 0;
-	/* this loop writes the string to video memory */
-	while (data[j] != '\0') {
-		/* the character's ascii */
-		vidptr[i] = data[j];
-		/* attribute-byte: give character black bg and light grey fg */
-		vidptr[i+1] = 0x07;
-		++j;
-		i = i + 2;
+	size_t size = strlen(data);
+	for (size_t i = 0; i < size; i++) {
+		terminal_putchar(data[i]);
 	}
 	return;
 }
