1397b4170f8408a94789ce0055bd86d693ec1432
Calgary Michael <cseth.michael@gmail.com>
2021-03-13 15:35:39 -0500

    more helpful debugging output

---
+++ libs/kernel/include/kernel/debug.h
@@ -0,0 +1,7 @@
+#ifndef _KERNEL_DEBUG_H
+#define _KERNEL_DEBUG_H
+
+void debug_output(void);
+
+#endif
+
--- libs/kernel/kernel/kernel.c
+++ libs/kernel/kernel/kernel.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
+#include <kernel/debug.h>
 #include <kernel/segmentation.h>
 #include <kernel/tty.h>
 
 void kmain(void) {
 	terminal_init();
+	
+	terminal_out("Hello, kernel\n\n\0");
+	debug_output();
+	terminal_out("\n\n\0");
 
 	initialize_segments();
-	
-	terminal_out("Hello, kernel\n\0");
-	terminal_out("This is a new line\n\0");
-	terminal_out("This is a\ttab\n\0");
-	printf("Here are some numbers: %d - %d - %d - %d - %d - %d - %d - %d - %d - %d\n", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 }
 
